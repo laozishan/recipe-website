a1eba167e2594374a049634fe96373a1b629afa5
laozishan <842714355@qq.comn>
2023-01-19 17:59:08 +0800

    added api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'cd92c684-9398-4cd9-8cd1-43d43f985ee3';
 export const MODAL_CLOSE_SEC = 2.5;
